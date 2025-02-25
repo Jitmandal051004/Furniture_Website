7cb615fee1581e9eac43eedc55a3915f3cbc5692
Jitmandal051004 <102886330+Jitmandal051004@users.noreply.github.com>
2025-02-25 18:59:20 +0530

    service.tsx changes and build done

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -11,15 +11,34 @@ const Service = () => {
                     <span><Link className='hover:text-gray-500 border-b-2 border-b-black hover:border-b-gray-500' href={"/"}>Explore</Link></span>
                 </div>
                 <div className="grid grid-cols-2 gap-4">
-                    {ServicesP.map((ser,index)=>(
-                        <div key={index} className={`${ser.img} relative overflow-hidden bg-no-repeat bg-[50%_80%] ${ser.position} h-[520px] w-[792px] flex justify-center`}>
-                            <div className="absolute inset-0 bg-black opacity-45"></div>
-                            <div className={`${roboto_slab} flex relative flex-col items-center gap-3 text-white top-[376px]`}>
-                                <span className='font-semibold'>{ser.content}</span>
-                                <span><Link className='text-sm hover:text-gray-300 border-b-2 border-b-white hover:border-b-gray-300' href={"/"}>Explore</Link></span>
-                            </div>
+                    <div className={`bg-CP1 relative overflow-hidden bg-no-repeat bg-[50%_80%] bg-[length:912px_1035px] h-[520px] w-[792px] flex justify-center`}>
+                        <div className="absolute inset-0 bg-black opacity-45"></div>
+                        <div className={`${roboto_slab} flex relative flex-col items-center gap-3 text-white top-[376px]`}>
+                            <span className='font-semibold'>Custom Sofas</span>
+                            <span><Link className='text-sm hover:text-gray-300 border-b-2 border-b-white hover:border-b-gray-300' href={"/"}>Explore</Link></span>
                         </div>
-                    ))}
+                    </div>
+                    <div className={`bg-CP2 relative overflow-hidden bg-no-repeat bg-[50%_80%] bg-[length:912px_1035px] h-[520px] w-[792px] flex justify-center`}>
+                        <div className="absolute inset-0 bg-black opacity-45"></div>
+                        <div className={`${roboto_slab} flex relative flex-col items-center gap-3 text-white top-[376px]`}>
+                            <span className='font-semibold'>Curtain works</span>
+                            <span><Link className='text-sm hover:text-gray-300 border-b-2 border-b-white hover:border-b-gray-300' href={"/"}>Explore</Link></span>
+                        </div>
+                    </div>
+                    <div className={`bg-CP3 relative overflow-hidden bg-no-repeat bg-cover h-[520px] w-[792px] flex justify-center`}>
+                        <div className="absolute inset-0 bg-black opacity-45"></div>
+                        <div className={`${roboto_slab} flex relative flex-col items-center gap-3 text-white top-[376px]`}>
+                            <span className='font-semibold'>Wallpapers</span>
+                            <span><Link className='text-sm hover:text-gray-300 border-b-2 border-b-white hover:border-b-gray-300' href={"/"}>Explore</Link></span>
+                        </div>
+                    </div>
+                    <div className={`bg-CP4 relative overflow-hidden bg-no-repeat bg-[length:912px_1035px] bg-[50%_80%] h-[520px] w-[792px] flex justify-center`}>
+                        <div className="absolute inset-0 bg-black opacity-45"></div>
+                        <div className={`${roboto_slab} flex relative flex-col items-center gap-3 text-white top-[376px]`}>
+                            <span className='font-semibold'>Window Blinds</span>
+                            <span><Link className='text-sm hover:text-gray-300 border-b-2 border-b-white hover:border-b-gray-300' href={"/"}>Explore</Link></span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
